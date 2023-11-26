0fec3c2496f4432c287ce68d2c931295cf3e421d
Iryna Burlaka <i.am.blinova@gmail.com>
2023-11-26 19:00:30 +0100

    complited ts-hw-03

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,39 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  public tenants: Person[] = [];
+
+  constructor(public key: Key) {}
+
+  comeIn(newTenant: Person): void {
+    if (this.door) {
+      this.tenants.push(newTenant);
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(externalKey: Key): void {
+    if (externalKey === this.key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +42,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
